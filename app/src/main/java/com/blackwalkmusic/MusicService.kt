32e6cf1cdb35226bdb1adcde1183429cc828b7e2
medinam-code <konnomedinam@gmail.com>
2026-08-14 19:26:49 -0300

    Enhance MusicService with session activity and player setup
    Updated MusicService to include session activity and improved player initialization.

--- app/src/main/java/com/blackwalkmusic/MusicService.kt
+++ app/src/main/java/com/blackwalkmusic/MusicService.kt
@@ -1,5 +1,8 @@
 package com.blackwalkmusic
 
+import android.app.PendingIntent
+import android.content.ComponentName
+import android.content.Intent
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
 
@@ -10,23 +13,58 @@ class MusicService : MediaSessionService() {
     override fun onCreate() {
         super.onCreate()
 
-        val player = MusicPlayer.getPlayer(applicationContext)
+        val player =
+            MusicPlayer.getPlayer(this)
 
-        mediaSession = MediaSession.Builder(
-            this,
-            player
-        ).build()
+        val activityComponent =
+            ComponentName(
+                packageName,
+                "com.blackwalkmusic.MainActivity"
+            )
+
+        val sessionIntent =
+            Intent().apply {
+
+                component =
+                    activityComponent
+
+                addFlags(
+                    Intent.FLAG_ACTIVITY_SINGLE_TOP or
+                        Intent.FLAG_ACTIVITY_CLEAR_TOP
+                )
+            }
+
+        val sessionActivity =
+            PendingIntent.getActivity(
+                this,
+                0,
+                sessionIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT or
+                    PendingIntent.FLAG_IMMUTABLE
+            )
+
+        mediaSession =
+            MediaSession.Builder(
+                this,
+                player
+            )
+                .setSessionActivity(
+                    sessionActivity
+                )
+                .build()
     }
 
     override fun onGetSession(
         controllerInfo: MediaSession.ControllerInfo
     ): MediaSession? {
+
         return mediaSession
     }
 
     override fun onDestroy() {
 
         mediaSession?.release()
+
         mediaSession = null
 
         MusicPlayer.release()
