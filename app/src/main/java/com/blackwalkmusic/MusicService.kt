bb98f2a8f3ed42bc5dbddfa5a202c79271d432d4
medinam-code <konnomedinam@gmail.com>
2026-08-14 19:07:08 -0300

    Refactor MusicService to use application context

--- app/src/main/java/com/blackwalkmusic/MusicService.kt
+++ app/src/main/java/com/blackwalkmusic/MusicService.kt
@@ -1,8 +1,5 @@
 package com.blackwalkmusic
 
-import android.app.PendingIntent
-import android.content.ComponentName
-import android.content.Intent
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
 
@@ -13,42 +10,12 @@ class MusicService : MediaSessionService() {
     override fun onCreate() {
         super.onCreate()
 
-        val player = MusicPlayer.getPlayer(this)
-
-        /*
-         * Abrimos MainActivity mediante su nombre completo.
-         *
-         * Esto evita que MusicService tenga que importar o
-         * resolver directamente la clase MainActivity.
-         */
-        val activityComponent = ComponentName(
-            packageName,
-            "com.blackwalkmusic.MainActivity"
-        )
-
-        val sessionIntent = Intent().apply {
-            component = activityComponent
-
-            addFlags(
-                Intent.FLAG_ACTIVITY_SINGLE_TOP or
-                    Intent.FLAG_ACTIVITY_CLEAR_TOP
-            )
-        }
-
-        val sessionActivity = PendingIntent.getActivity(
-            this,
-            0,
-            sessionIntent,
-            PendingIntent.FLAG_UPDATE_CURRENT or
-                PendingIntent.FLAG_IMMUTABLE
-        )
+        val player = MusicPlayer.getPlayer(applicationContext)
 
         mediaSession = MediaSession.Builder(
             this,
             player
-        )
-            .setSessionActivity(sessionActivity)
-            .build()
+        ).build()
     }
 
     override fun onGetSession(
